c78b38e33795602668cbb970c084dbc3c465273d
Anderson Coelho <anderson.coelho@allss.com.br>
2021-03-13 18:16:33 -0300

    Implementação de módulos específicos

---
+++ project_hours/__manifest__.py
@@ -0,0 +1,20 @@
+{
+  'name': 'Project hours',
+  'version': '11.0.1.0.0',
+  'summary': 'project hours',
+  'category': 'project',
+  'company':"ALLSS Soluções em Sistemas"
+  'website': 'https://allss.com.br',
+  'author': 'Rodrigo',
+  'depends': [
+    'helpdesk_timesheet',
+  ],
+
+  'data': [
+    # Views
+    'views/helpdesk_views.xml',
+  ],
+  'installable': True,
+  'auto_install': False,
+  'application': False,
+}
